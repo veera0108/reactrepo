3b5c91fb5267a755797207bbcb1e2dfa1226484c
Veeravel <veera0108@gmail.com>
2023-05-15 14:16:20 +0530

    App.js changed

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        React - Git Repo App
+        React - Git Repo App - by Veera
       </header>
     </div>
   );
