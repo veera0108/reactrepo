710757d69509652e597724e26418576ce112ea9d
Veeravel <veera0108@gmail.com>
2023-05-17 14:50:23 +0530

    Revert "app changes"
    This reverts commit c1d4676063ccf8803e091b1ad1cd360667ceb156.

--- src/App.js
+++ src/App.js
@@ -5,8 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        React - Git Repo App - (by Veera)...
-        modified.....
+        React - Git Repo App - by Veera....HITS
       </header>
     </div>
   );
